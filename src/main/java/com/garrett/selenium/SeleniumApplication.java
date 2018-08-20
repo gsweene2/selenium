c569b2dc18a8d59bac3f355ed92712001c02fa8c
gsweene2 <gsweene2@u.rochester.edu>
2018-08-20 01:14:34 -0700

    Automate test run on app startup

--- src/main/java/com/garrett/selenium/SeleniumApplication.java
+++ src/main/java/com/garrett/selenium/SeleniumApplication.java
@@ -1,7 +1,14 @@
 package com.garrett.selenium;
 
+import com.garrett.selenium.BookExample.BestPractises3;
+import org.junit.internal.TextListener;
+import org.junit.runner.JUnitCore;
+import org.junit.runner.Result;
+import org.junit.runner.notification.Failure;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.List;
 
 @SpringBootApplication
 public class SeleniumApplication {
@@ -9,5 +16,23 @@ public class SeleniumApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SeleniumApplication.class, args);
 		System.setProperty("webdriver.gecko.driver","src/main/resources/geckodriver");
+
+        JUnitCore junit = new JUnitCore();
+        junit.addListener(new TextListener(System.out));
+        Result result = junit.run(BestPractises3.class);
+
+        /*
+        List<Failure> failures = result.getFailures();
+        for (Failure fail:failures){
+            System.out.println("-------------------");
+            System.out.println(fail.getTestHeader());
+            System.out.println(fail.getException());
+            System.out.println(fail.getDescription());
+        }
+        if (failures.size() == 0){
+            System.out.println("-------------------");
+            System.out.println("All tests Succeeded!");
+        }
+        */
 	}
 }
